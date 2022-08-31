1b5bc879d4a28aca391fc986334f22f3dcc298ef
Christopher Klint <christopher.klint@gmail.com>
2022-08-31 22:17:57 +0200

    feat: starting to add translations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import { GlobalStyles } from "@/lib/GlobalStyles";
+import { appWithTranslation } from "next-i18next";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,4 +11,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp;
+export default appWithTranslation(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import Image from "next/image";
 import styled from "styled-components";
 import turntable from "@/public/images/turntable.jpg";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 const Title = styled.h1`
   font-weight: 600;
@@ -10,19 +11,50 @@ const Title = styled.h1`
   color: #e5e7eb;
 `;
 
-const Page = styled.section`
+const Subtitle = styled.h2`
+  font-weight: 600;
+  font-size: 1.875rem;
+  line-height: 2.25rem;
+  color: #e5e7eb;
+`;
+
+const Page = styled.main`
   max-width: 1280px;
   margin: 0 auto;
   padding: 1rem;
 `;
 
+const Section = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Content = styled.div`
+  position: absolute;
+  z-index: 1;
+`;
+
 const Home: NextPage = () => {
   return (
     <Page>
-      <Title>This is the homepage.</Title>
-      <Image src={turntable} alt="" placeholder="blur" />
+      <Section>
+        <Image src={turntable} alt="" placeholder="blur" />
+        <Content>
+          <Title>DJ Klint</Title>
+          <Subtitle></Subtitle>
+        </Content>
+      </Section>
     </Page>
   );
+};
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale || "", ["home"])),
+    },
+  };
 };
 
 export default Home;
